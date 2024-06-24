a9f4522201c6f80f66e81b8367e78408af887740
ikumisato <kinoko7971@gmail.com>
2024-06-25 06:08:34 +0900

    関数を作成

---
+++ js/function.js
@@ -0,0 +1,16 @@
+// 朝のあいさつを出力する関数を定義する
+const sayGoodMorning = () => {
+  console.log("おはようございます");
+  console.log("昨日はよく蒸れましたか？");
+  console.log("今日も一日頑張りましょう！");
+};
+// 夜のあいさつを出力する関数を定義する
+const sayGoodEvening = () => {
+  console.log("こんばんは！");
+  console.log("今日も一日お疲れ様でした。");
+};
+
+// 朝のあいさつを出力する関数を呼び出す
+sayGoodMorning();
+
+sayGoodMorning();
