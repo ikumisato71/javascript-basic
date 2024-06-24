66ab75b69a70cc1d2173216e97097747a639826d
ikumisato <kinoko7971@gmail.com>
2024-06-25 05:22:21 +0900

    オブジェクトを作成

---
+++ js/object.js
@@ -0,0 +1,15 @@
+// オブジェクトの宣言と値の代入を行う
+const parsonalData = { name: "侍太郎", age: 36, gender: "男性" };
+// オブジェクトの値を出力する
+console.log(parsonalData);
+
+// 'age'というキーの値を更新する
+parsonalData.age = 37;
+
+// 新しくプロパティ（キーと値）を追加する
+parsonalData.address = "東京都";
+
+// オブジェクトの値を出力する
+console.log(parsonalData);
+// 'gender'というキーの値を出力する
+console.log(parsonalData.gender);
